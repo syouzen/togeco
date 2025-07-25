dbdfea9a9df8657a2552dcda6ca4e7d497cefbc3
syouzen <seungyeon.choi1026@gmail.com>
2025-07-26 00:53:02 +0900

    feat: first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Togeco - 함께 공유하자",
+  description: "함께 공유하자. togeco",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
